e907829ceae4c3bb45326a9c19822baafa43a99f
堀航大 <kodenginner4521@gmail.com>
2021-04-13 11:25:34 +0900

    メールアドレスログイン機能搭載

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import styles from "./Auth.module.css";
 import { auth, provider, storage } from "../firebase";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import SendIcon from "@material-ui/icons/Send";
+import CameraIcon from "@material-ui/icons/Camera";
+import EmailIcon from "@material-ui/icons/Email";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { error } from "node:console";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -32,16 +37,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', 
+    width: "100%",
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -51,9 +56,29 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth: React.FC = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const signInEmail = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      alert("メールアドレスまたはパスワードが正しくありません");
+    }
+  };
+
+  const signUpEmail = async () => {
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   const signInGoogle = async () => {
-    await auth.signInWithPopup(provider).catch((err) => alert(err.message))
-  }
+    await auth.signInWithPopup(provider).catch((err) => alert(err.message));
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -64,7 +89,7 @@ const Auth: React.FC = () => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          {isLogin ? "ログイン" : "新規アカウント登録"}
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -73,10 +98,14 @@ const Auth: React.FC = () => {
             required
             fullWidth
             id="email"
-            label="Email Address"
+            label="メールアドレス"
             name="email"
             autoComplete="email"
             autoFocus
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value);
+            }}
           />
           <TextField
             variant="outlined"
@@ -84,10 +113,14 @@ const Auth: React.FC = () => {
             required
             fullWidth
             name="password"
-            label="Password"
+            label="パスワード"
             type="password"
             id="password"
             autoComplete="current-password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
+            }}
           />
           <Button
             type="submit"
@@ -95,23 +128,49 @@ const Auth: React.FC = () => {
             variant="contained"
             color="primary"
             className={classes.submit}
+            startIcon={<EmailIcon />}
+            onClick={(e) => {
+              e.preventDefault();
+              isLogin ? signInEmail() : signUpEmail();
+            }}
           >
-            Sign In
+            {isLogin ? "ログイン" : "新規アカウント登録"}
           </Button>
+          <Grid className={styles.login_bar}>または</Grid>
           <Button
             fullWidth
             variant="contained"
             color="primary"
-            className={classes.submit} 
+            className={classes.submit}
+            startIcon={<CameraIcon />}
             onClick={signInGoogle}
           >
             Googleアカウントでログイン
           </Button>
+          <Grid>
+            {isLogin ? (
+              <p className={styles.login_toggle}>
+                アカウントはお持ちでないですか？
+                <span
+                  className={styles.login_toggleMode}
+                  onClick={() => setIsLogin(false)}
+                >
+                  登録する
+                </span>
+              </p>
+            ) : (
+              <p
+                className={styles.login_right}
+                onClick={() => setIsLogin(true)}
+              >
+                ログインに戻る
+              </p>
+            )}
+          </Grid>
         </form>
       </div>
-  
     </Container>
   );
-}
+};
 
 export default Auth;
