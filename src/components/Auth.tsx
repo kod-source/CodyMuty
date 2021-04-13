d5dc75c45cda49b2e04f4d5406f4010c473279a7
堀航大 <kodenginner4521@gmail.com>
2021-04-13 12:55:44 +0900

    新規ユーザーの登録機能

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -20,19 +20,8 @@ import CameraIcon from "@material-ui/icons/Camera";
 import EmailIcon from "@material-ui/icons/Email";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { error } from "node:console";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import { updateUserProfile } from "../features/userSlice";
+import { IconButton } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -56,9 +45,20 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth: React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [avatarImage, setAvatarImage] = useState<File | null>(null);
   const [isLogin, setIsLogin] = useState(true);
+
+  const changeAvatarImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setAvatarImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
 
   const signInEmail = async () => {
     try {
@@ -70,7 +70,35 @@ const Auth: React.FC = () => {
 
   const signUpEmail = async () => {
     try {
-      await auth.createUserWithEmailAndPassword(email, password);
+      const authUser = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      let url = "";
+      if (avatarImage) {
+        const S =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        const N = 16;
+        const randomChar = Array.from(
+          crypto.getRandomValues(new Uint32Array(N))
+        )
+          .map((n) => [n % S.length])
+          .join("");
+        const fileName = randomChar + "_" + avatarImage.name;
+
+        await storage.ref(`avatarts/${fileName}`).put(avatarImage);
+        url = await storage.ref("avatarts").child(fileName).getDownloadURL();
+      }
+      await authUser.user?.updateProfile({
+        displayName: username,
+        photoURL: url,
+      });
+      dispatch(
+        updateUserProfile({
+          displayName: username,
+          photoUrl: url,
+        })
+      );
     } catch (error) {
       alert(error.message);
     }
@@ -92,6 +120,44 @@ const Auth: React.FC = () => {
           {isLogin ? "ログイン" : "新規アカウント登録"}
         </Typography>
         <form className={classes.form} noValidate>
+          {!isLogin && (
+            <>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="ユーザーネーム"
+                name="username"
+                autoComplete="usename"
+                value={username}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setUsername(e.target.value);
+                }}
+              />
+              <Box textAlign="center">
+                <IconButton>
+                  <label>
+                    <AccountCircleIcon
+                      fontSize="large"
+                      className={
+                        avatarImage
+                          ? styles.login_addIconLoaded
+                          : styles.login_addIcon
+                      }
+                    />
+                    <input
+                      className={styles.login_hiddenIcon}
+                      type="file"
+                      onChange={changeAvatarImage}
+                    />
+                  </label>
+                </IconButton>
+              </Box>
+            </>
+          )}
+
           <TextField
             variant="outlined"
             margin="normal"
@@ -122,7 +188,33 @@ const Auth: React.FC = () => {
               setPassword(e.target.value);
             }}
           />
+          {!isLogin && (
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="checkpassword"
+              label="確認のためもう一度、入力してください"
+              type="password"
+              id="ckeckpassword"
+              autoComplete="current-password"
+              value={checkPassword}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setCheckPassword(e.target.value);
+              }}
+            />
+          )}
           <Button
+            disabled={
+              isLogin
+                ? !email || password.length < 6
+                : !username ||
+                  !email ||
+                  password.length < 6 ||
+                  !avatarImage ||
+                  password !== checkPassword
+            }
             type="submit"
             fullWidth
             variant="contained"
@@ -147,6 +239,9 @@ const Auth: React.FC = () => {
           >
             Googleアカウントでログイン
           </Button>
+          <Grid className={styles.login_reset}>
+            <span>パスワードを忘れた場合</span>
+          </Grid>
           <Grid>
             {isLogin ? (
               <p className={styles.login_toggle}>
